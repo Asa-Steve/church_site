52cd6a36eb73508524c542ee7b0a3abab4b6b83f
Asadu Steve <asadusteve456@gmail.com>
2024-12-18 19:17:10 +0100

    removed unnecessary  borders from dashboard

--- client/src/pages/Admin_pages/Dashboard/Dashboard.jsx
+++ client/src/pages/Admin_pages/Dashboard/Dashboard.jsx
@@ -421,13 +421,13 @@ const Dashboard = () => {
                   <div className="plot">
                     <ResponsiveContainer width="100%" height="100%">
                       <BarChart
-                        width={500}
-                        height={300}
+                        width={100}
+                        height={100}
                         data={data}
                         margin={{
                           top: 5,
                           right: 30,
-                          left: 20,
+                          left: -10,
                           bottom: 5,
                         }}
                       >
