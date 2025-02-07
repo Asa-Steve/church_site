35265436fcb892530ae47234213758bc931e1a66
Asadu Steve <asadusteve456@gmail.com>
2025-02-07 21:26:13 +0100

    added a 404 page

--- client/src/pages/Error/Error.jsx
+++ client/src/pages/Error/Error.jsx
@@ -1,13 +1,27 @@
-import React from 'react';
+import React from "react";
 import "./Error.scss";
+import { Link } from "react-router-dom";
 
 const Error = () => {
-    return (
-        <>
-            <h1>Oops!</h1>
-            <p>Something went wrong</p>
-        </>
-    );
-}
+  return (
+    <>
+      <div className="error_wrap">
+        <div className="error_overlay"></div>
+        <div className="write_up">
+          <div className="top">
+            <span>404</span>
+            <span>Page Not Found</span>
+          </div>
+          <div className="bottom">
+            <span>
+              {" "}
+              return to <Link to={"/"}>homepage</Link>
+            </span>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
 
 export default Error;
