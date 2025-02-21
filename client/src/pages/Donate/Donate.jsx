a6d2ffbf795139a94d309da5912a7b20ba4d1a4e
Asadu Steve <asadusteve456@gmail.com>
2025-02-22 08:13:19 +1000

    added some texts to the donate page

--- client/src/pages/Donate/Donate.jsx
+++ client/src/pages/Donate/Donate.jsx
@@ -79,6 +79,63 @@ const Donate = () => {
           />
 
           <section className="donate form-section">
+            <div className="intro_text">
+              <div>
+                <h2>Support St. Matthias Catholic Church</h2>
+                <p>
+                  Your generosity helps sustain the mission of St. Matthias
+                  Catholic Church, enabling us to continue spreading the Gospel,
+                  supporting parish ministries, and serving those in need. Every
+                  contribution, no matter the size, makes a meaningful impact.
+                </p>
+              </div>
+              <div>
+                <h2>Why Donate?</h2>
+                <ul>
+                  <li>
+                    <strong>Support Parish Activities –</strong>Help fund
+                    liturgical celebrations, pastoral programs, and community
+                    outreach.
+                  </li>
+                  <li>
+                    <strong>Church Building Project –</strong>Be a part of the
+                    growth of our parish by contributing to the construction and
+                    improvement of our church facilities. Your donation helps
+                    create a sacred space for worship, prayer, and community
+                    gatherings.
+                  </li>
+                  <li>
+                    <strong>Assist the Less Privileged – </strong> Your
+                    donations help provide aid to those in need within our
+                    community.
+                  </li>
+                  <li>
+                    <strong>Maintain Our Church –</strong>Contribute to the
+                    upkeep of our place of worship and facilities.
+                  </li>
+                </ul>
+              </div>
+              <div>
+                <h2>Ways to Give</h2>
+                <ul>
+                  <li>
+                    <strong>Online Giving –</strong>Use the secure form on this
+                    page to make a one-time donation.
+                  </li>
+                  <li>
+                    <strong>Visit the Parish Office –</strong>Stop by the church
+                    office to make your donation in person.
+                  </li>
+                </ul>
+              </div>
+              <div>
+                <p>
+                  You can make direct bank transfers to either of these account
+                  numbers
+                </p>
+                <span>9167670473</span>
+              </div>
+            </div>
             <div className="wrap">
               <div className="form-header">
                 <h2>Donation Form</h2>
